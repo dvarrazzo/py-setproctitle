ac061dc79bc3b86482e0df7d2140e166829a5e40
Daniele Varrazzo <piro@develer.com>
2009-12-01 21:09:59 +0000

    sptdemo shows the process PID.

--- tools/spt_demo.c
+++ tools/spt_demo.c
@@ -1,18 +1,22 @@
+/* A small demo to show how to use the display change */
+
 #include "../src/spt_status.h"
 
 #include <stdio.h>
+#include <unistd.h>
 
 int
 main(int argc, char **argv)
 {
+    printf("Process PID: %i\n", getpid());
+
     argv = save_ps_display_args(argc, argv);
     init_ps_display("hello, world");
-
-    printf("title changed, press a key\n");
+    printf("Title changed, press enter\n");
     getchar();
 
     set_ps_display("new title!", true);
-    printf("title changed again, press a key to exit\n");
+    printf("Title changed again, press enter to exit\n");
     getchar();
 
     return 0;
