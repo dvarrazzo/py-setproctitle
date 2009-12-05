ee4b27c4d8b9f13bb841f33e21b432e42bf925f1
Daniele Varrazzo <piro@develer.com>
2009-12-05 19:40:48 +0000

    Google code ok, PyPI ok... Version bumped to 0.1\!

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Copyright (c) 2009 Daniele Varrazzo <daniele.varrazzo@gmail.com>
 import sys
 from distutils.core import setup, Extension
 
-VERSION = '0.1a0'
+VERSION = '0.1'
 
 mod_spt = Extension('setproctitle',
     define_macros=[('SPT_VERSION', '"%s"' % VERSION),],
