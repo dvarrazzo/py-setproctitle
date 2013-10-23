335bd8bf3f255e1aa929f9e278224648da33c58f
Jason Myers <jason@jasonamyers.com>
2013-10-23 06:35:59 -0500

    Switched to using supplied spt_strlcpy, changed spt_status.c to use it, and removed option to use system strlcpy from c.h
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- src/c.h
+++ src/c.h
@@ -29,14 +29,6 @@ typedef char bool;
 
 #include <stddef.h>
 
-#ifdef strlcpy
-#undef strlcpy
-#endif
-
-#if !HAVE_DECL_STRLCPY
-HIDDEN extern size_t strlcpy(char *dst, const char *src, size_t siz);
-#endif
-
 #ifdef WIN32
 #include <Windows.h>
 #endif
