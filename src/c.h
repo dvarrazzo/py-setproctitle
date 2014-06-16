5c55e00b7c8b095bb380d9190a7c38079ed7d336
Patrick Stewart <patstew@gmail.com>
2014-06-16 15:21:42 +0100

    Fix build on Windows with MS compiler

--- src/c.h
+++ src/c.h
@@ -32,7 +32,7 @@ typedef char bool;
 /* Let's use our version of strlcpy to avoid portability problems */
 size_t spt_strlcpy(char *dst, const char *src, size_t siz);
 
-#ifdef WIN32
+#ifdef _WIN32
 #include <Windows.h>
 #endif
 
