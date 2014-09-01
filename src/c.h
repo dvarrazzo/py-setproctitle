70cc6a2b88bf09e57d11e0356f817bd72ef351f6
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2014-09-01 07:34:46 +0100

    Tentative fix for the VC compiling problems
    Inspired by https://github.com/patstew/py-setproctitle/commit/5c55e00b

--- src/c.h
+++ src/c.h
@@ -32,6 +32,13 @@ typedef char bool;
 /* Let's use our version of strlcpy to avoid portability problems */
 size_t spt_strlcpy(char *dst, const char *src, size_t siz);
 
+/* VC defines _WIN32, not WIN32 */
+#ifdef _WIN32
+#ifndef WIN32
+#define WIN32 _WIN32
+#endif
+#endif
+
 #ifdef WIN32
 #include <Windows.h>
 #endif
