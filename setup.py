12b8387b29df5f6e69144a7390221aee6d42aaf5
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-06-27 02:15:14 +1200

    setup blacker than black

--- setup.py
+++ setup.py
@@ -13,82 +13,87 @@ except ImportError:
     from distutils.core import setup, Extension
 
 
-VERSION = '1.2.dev0'
+VERSION = "1.2.dev0"
 
 
 define_macros = {}
 
-define_macros['SPT_VERSION'] = VERSION
+define_macros["SPT_VERSION"] = VERSION
 
-if sys.platform.startswith('linux'):
-    define_macros['HAVE_SYS_PRCTL_H'] = 1
+if sys.platform.startswith("linux"):
+    define_macros["HAVE_SYS_PRCTL_H"] = 1
 
-elif sys.platform == 'darwin':
+elif sys.platform == "darwin":
     # __darwin__ symbol is not defined; __APPLE__ is instead.
-    define_macros['__darwin__'] = 1
+    define_macros["__darwin__"] = 1
 
-elif 'bsd' in sys.platform:     # OMG, how many of them are?
-    define_macros['BSD'] = 1
-    define_macros['HAVE_SETPROCTITLE'] = 1
-    define_macros['HAVE_PS_STRING'] = 1
+elif "bsd" in sys.platform:  # OMG, how many of them are?
+    define_macros["BSD"] = 1
+    define_macros["HAVE_SETPROCTITLE"] = 1
+    define_macros["HAVE_PS_STRING"] = 1
 
 # NOTE: the module may work on HP-UX using pstat
 # thus setting define_macros['HAVE_SYS_PSTAT_H']
 # see http://www.noc.utoronto.ca/~mikep/unix/HPTRICKS
 # But I have none handy to test with.
 
-mod_spt = Extension('setproctitle',
+mod_spt = Extension(
+    "setproctitle",
     define_macros=list(define_macros.items()),
     sources=[
-        'src/setproctitle.c',
-        'src/spt_debug.c',
-        'src/spt_setup.c',
-        'src/spt_status.c',
-        'src/spt_strlcpy.c',
-    ])
+        "src/setproctitle.c",
+        "src/spt_debug.c",
+        "src/spt_setup.c",
+        "src/spt_status.c",
+        "src/spt_strlcpy.c",
+    ],
+)
 
 # patch distutils if it can't cope with the "classifiers" or
 # "download_url" keywords
-if sys.version < '2.2.3':
+if sys.version < "2.2.3":
     from distutils.dist import DistributionMetadata
+
     DistributionMetadata.classifiers = None
     DistributionMetadata.download_url = None
 
 # Try to include the long description in the setup
 kwargs = {}
 try:
-    kwargs['long_description'] = (
-        open('README.rst').read() +
-        '\n' +
-        open('HISTORY.rst').read())
-except:
+    kwargs["long_description"] = (
+        open("README.rst").read() + "\n" + open("HISTORY.rst").read()
+    )
+except Exception:
     pass
 
+classifiers = """\
+Development Status :: 5 - Production/Stable
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Programming Language :: C
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Operating System :: POSIX :: Linux
+Operating System :: POSIX :: BSD
+Operating System :: MacOS :: MacOS X
+Operating System :: Microsoft :: Windows
+Topic :: Software Development
+""".splitlines()
+
 setup(
-    name='setproctitle',
-    description='A Python module to customize the process title',
+    name="setproctitle",
+    description="A Python module to customize the process title",
     version=VERSION,
-    author='Daniele Varrazzo',
-    author_email='daniele.varrazzo@gmail.com',
-    url='https://github.com/dvarrazzo/py-setproctitle',
-    download_url='http://pypi.python.org/pypi/setproctitle/',
-    license='BSD',
-    platforms=['GNU/Linux', 'BSD', 'MacOS X', 'Windows'],
+    author="Daniele Varrazzo",
+    author_email="daniele.varrazzo@gmail.com",
+    url="https://github.com/dvarrazzo/py-setproctitle",
+    download_url="http://pypi.python.org/pypi/setproctitle/",
+    license="BSD",
+    platforms=["GNU/Linux", "BSD", "MacOS X", "Windows"],
     python_requires=">=3.6",
-    classifiers=[r for r in map(str.strip, """
-        Development Status :: 5 - Production/Stable
-        Intended Audience :: Developers
-        License :: OSI Approved :: BSD License
-        Programming Language :: C
-        Programming Language :: Python :: 3
-        Programming Language :: Python :: 3.6
-        Programming Language :: Python :: 3.7
-        Programming Language :: Python :: 3.8
-        Operating System :: POSIX :: Linux
-        Operating System :: POSIX :: BSD
-        Operating System :: MacOS :: MacOS X
-        Operating System :: Microsoft :: Windows
-        Topic :: Software Development
-        """.splitlines()) if r],
+    classifiers=classifiers,
     ext_modules=[mod_spt],
-    **kwargs)
+    **kwargs
+)
