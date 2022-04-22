f9ff07e0cf841f7af9422f8e6acad4e41a67b676
Eugene Gershnik <gershnik@hotmail.com>
2022-04-22 10:17:46 -0700

    Marking new darwin_set_process_title HIDDEN

--- src/darwin_set_process_name.h
+++ src/darwin_set_process_name.h
@@ -1,8 +1,10 @@
 #ifndef HEADER_DARWIN_SET_PROCESS_NAME_H_INCLUDED
 #define HEADER_DARWIN_SET_PROCESS_NAME_H_INCLUDED
 
+#include "spt_config.h"
+
 #include <stdbool.h>
 
-bool darwin_set_process_title(const char * title);
+HIDDEN bool darwin_set_process_title(const char * title);
 
 #endif
